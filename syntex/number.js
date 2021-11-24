53b66105557249bd6f6943728c5784a06d632fe3
thoongee <94193480+thoongee@users.noreply.github.com>
2021-11-24 10:56:28 +0900

    Add files via upload

---
+++ syntex/number.js
@@ -0,0 +1,2 @@
+//실행방법: 콘솔창에 node number.js 입력
+console.log(1+1); //숫자표현, + - * / 가능
---
+++ syntex/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('dkfj qokdjg qekjgkd adfj qgkdo qkejg ckald giqjdkg qoie gkdjaofkdj dkfj.'.length);
